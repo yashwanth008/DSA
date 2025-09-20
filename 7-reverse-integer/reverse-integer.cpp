116b15294c553756e901396bd05fb23c2a85bc92
yashwanth008 <76914360+yashwanth008@users.noreply.github.com>
2025-09-20 09:18:12 -0700

    Time: 2 ms (39.69%) | Memory: 8.5 MB (82.20%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -5,7 +5,9 @@ public:
         while(x != 0){
             int last = x%10;
             x /= 10;
-            if(rev > INT_MAX/10 || (rev == INT_MAX/10 && last > 7) || rev < INT_MIN/10 || (rev == INT_MIN/10 && last < -8)) return 0;
+            if(rev > INT_MAX/10 || (rev == INT_MAX/10 && last > 7)) return 0;
+            if(rev < INT_MIN/10 || (rev == INT_MIN/10 && last < -8)) return 0;
+
             rev = rev * 10 + last;
         }
         return rev;
