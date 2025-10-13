b2e98ad419684ffa44efee9e08ba8d01bade2be5
yashwanth008 <76914360+yashwanth008@users.noreply.github.com>
2025-10-13 02:59:31 -0700

    Time: 0 ms (100.00%) | Memory: 9.2 MB (68.37%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -33,3 +33,56 @@ public:
         return result;
     }
 };
+
+// google interview question
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // Recursive function to decode the string starting at index 'i'
+// string decode(const string &s, int &i) {
+//     string result = "";
+    
+//     while (i < s.size() && s[i] != ')') {
+//         if (s[i] == '(') {
+//             i++; // Skip '('
+//             string inner = decode(s, i); // Decode inside parentheses
+//             i++; // Skip ')'
+            
+//             int repeat = 0;
+//             if (i < s.size() && s[i] == '{') {
+//                 i++; // Skip '{'
+//                 while (i < s.size() && isdigit(s[i])) {
+//                     repeat = repeat * 10 + (s[i] - '0');
+//                     i++;
+//                 }
+//                 i++; // Skip '}'
+//             } else {
+//                 repeat = 1; // Default repeat count
+//             }
+            
+//             // Append the repeated substring
+//             for (int j = 0; j < repeat; j++)
+//                 result += inner;
+//         } 
+//         else {
+//             // Normal character, just append
+//             result += s[i];
+//             i++;
+//         }
+//     }
+//     return result;
+// }
+
+// string decompress(string s) {
+//     int i = 0;
+//     return decode(s, i);
+// }
+
+// // Driver function
+// int main() {
+//     cout << decompress("a(bcd){3}e") << endl;        // Output: "abcdbcdbcde"
+//     cout << decompress("a(bc(d){4}e){3}f") << endl;  // Output: "abcddddebcddddebcddddef"
+//     return 0;
+// }
