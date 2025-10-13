dbc352caa100533b28b55d1dc65a48d700d9d191
yashwanth008 <76914360+yashwanth008@users.noreply.github.com>
2025-10-12 23:31:49 -0700

    Time: 7 ms (57.68%) | Memory: 17.5 MB (70.46%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    int numUniqueEmails(vector<string>& emails) {
+        unordered_map<string,int> unquie;  //checking uniqie domain and localname
+        for(string &email:emails){
+            int n = email.size();
+            string cleanEmail = "";
+            for(char &i:email){
+                if(i == '+' || i == '@') break;
+                if(i == '.') continue;
+                cleanEmail+=i;
+            }
+            int pos = email.rfind('@');
+            string domainName = email.substr(pos,n);
+            cleanEmail+=domainName;
+            cout<<cleanEmail;
+            if(!unquie.count(cleanEmail)) unquie[cleanEmail] = n;
+            
+        }
+        return unquie.size();
+    }
+};
