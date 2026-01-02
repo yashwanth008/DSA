b6bc31cb3641122b9678c9ba9fa97cf988abff87
yashwanth008 <76914360+yashwanth008@users.noreply.github.com>
2026-01-01 22:58:01 -0800

    Time: 0 ms (100.00%) | Memory: 8.9 MB (51.75%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.cpp
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        unordered_map<char,int> cowCount;
+        // methond 1 beats 28%
+
+        // unordered_map<char,int> cowCount;
+        // int x = 0,y = 0;
+        // for(int i = 0;i<secret.size();i++){
+        //     if(secret[i] == guess[i]) x++;
+        //     else{
+        //         cowCount[secret[i]]++;
+        //         //cout<<cowCount[secret[i]]<<i<<" ";
+        //     }
+        // }
+        // for(int j = 0;j<guess.size();j++){
+        //     char ch = guess[j];
+        //     if(cowCount.count(ch) && cowCount[ch] > 0 && secret[j] != ch){
+        //         cowCount[ch]--;
+        //         //cout<<cowCount[ch]<<ch<<" ";
+        //         y++;
+        //     }
+        // }
+        // return to_string(x) + "A" + to_string(y) + "B";
+
+        //method 2 
+
+        vector<int> sec(10);
+        vector<int> gue(10);
         int x = 0,y = 0;
         for(int i = 0;i<secret.size();i++){
             if(secret[i] == guess[i]) x++;
             else{
-                cowCount[secret[i]]++;
-                //cout<<cowCount[secret[i]]<<i<<" ";
+                sec[secret[i] - '0']++;
+                gue[guess[i] - '0']++;
             }
         }
-        for(int j = 0;j<guess.size();j++){
-            char ch = guess[j];
-            if(cowCount.count(ch) && cowCount[ch] > 0 && secret[j] != ch){
-                cowCount[ch]--;
-                //cout<<cowCount[ch]<<ch<<" ";
-                y++;
-            }
+        for(int j = 0;j<10;j++){
+            y += min(gue[j],sec[j]);
         }
         return to_string(x) + "A" + to_string(y) + "B";
+
     }
 };
