5c197eb7a07a964db333068ca362b96fea2a7206
yashwanth008 <76914360+yashwanth008@users.noreply.github.com>
2026-01-06 23:51:17 -0800

    Time: 2 ms (73.22%) | Memory: 10.9 MB (68.87%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int maps[26] = {0};
-        int i = 0,j = 0;
-        int maxFreq = 0;
-        int maxLen = INT_MIN;
-        while(j<s.size()){
-            maps[s[j] - 'A']++;
-            maxFreq = max(maxFreq,maps[s[j] - 'A']);
-            // seeing if this exe
-            if((j-i+1) - maxFreq > k){
-                maps[s[i]-'A']--;
-                i++;
-            }
-            maxLen = max(maxFreq,(j-i+1));
-            j++;
+       //two pointers
+       int i = 0,j = 0;
+       // a array to understand freq of each character;
+       int freq[26] = {0};
+       // understanding charLen;
+       int charLen = 0;
+       //obv
+       int maxLen = INT_MIN;
+       while(j<s.size()){
+        //freq of the char is counted
+        freq[s[j] - 'A']++;
+        // essentially picking max char len so that i can hold allways max char;
+        charLen = max(charLen,freq[s[j] - 'A']);
+        // now if k is exhausted and is exceeding you cant replace anymore
+        // if exceeded then decrease the first char size ( replish one k) and inc i drc window
+        if((j-i+1) - charLen > k){
+            freq[s[i]-'A']--;
+            i++;
         }
+        maxLen = max(charLen,(j-i+1));
+        j++;
+       }
         return maxLen;
-
     }
 };
