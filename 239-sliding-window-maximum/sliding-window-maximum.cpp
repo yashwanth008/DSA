e55b7445940ec6b9c66d89d5bde1cb5498283a24
yashwanth008 <76914360+yashwanth008@users.noreply.github.com>
2026-01-15 21:21:10 -0800

    Time: 15 ms (88.94%) | Memory: 139.2 MB (66.67%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -15,19 +15,18 @@ public:
        vector<int> result;
        deque<int>d;
        for(int i = 0;i < nums.size();i++){
-         // remove  from front when window moves to next frame
+         // decrease from front when window moves to next frame
          if(!d.empty() && d.front() == i - k) d.pop_front();
          // monotonic cond
          while(!d.empty() && nums[d.back()] < nums[i]) d.pop_back();
          // addin elms index condi
          d.push_back(i);
         // add the higest of window into result based on index
-        if (i >= k - 1) {
-            result.push_back(nums[d.front()]);
-        }
+        if (i >= k - 1) result.push_back(nums[d.front()]);
 
        }
        return result;
+
        
     }
 };
